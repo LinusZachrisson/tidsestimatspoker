728d47335b50772280324e0d48d325119c59f57b
Toni Halmetoja <36439969+ToniHalmetoja@users.noreply.github.com>
2021-10-13 12:37:18 +0200

    Create FetchIssue.js
    Prototype for issue fetcher. Returns a div with all numbers sorted, the average, and the median. Should work with the placeholder object replaced with anything else.

---
+++ src/components/FetchIssue.js
@@ -0,0 +1,30 @@
+function FetchIssue(number){
+
+    /*Send in the number of the issue you want to get, maybe*/
+    
+    let contents={
+        "issueId": 1,
+        "header": "Rubrik",
+        "description": "Förklarande text angående issuet",
+        "done": false,
+        "votes": [2, 4, 3, 7, 9, 5,10,15,3]
+    }
+    
+    const sum = contents.votes.reduce((a, b) => a + b, 0);
+    const avg = (sum / contents.votes.length) || 0;
+    const avgRounded = Math.round(avg * 100) / 100;
+    const sorted = contents.votes.slice().sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+    
+    return(
+        <div>
+            <h1>Issue: {contents.header}</h1>
+            <h2>Description: {contents.description}</h2>
+            All votes: <ul>{sorted.map(vote => <li> {vote} </li>)}</ul> <br/>
+            Average: {avgRounded}<br/>
+            Median: {sorted[middle]}
+        </div>
+    )
+    }
+    
+    export default FetchIssue
