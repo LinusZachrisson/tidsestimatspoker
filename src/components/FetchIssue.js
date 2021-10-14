c63d952e3b2945edda5ed8dcf705f9a707f93bf3
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2021-10-14 16:31:11 +0200

    Printar färdig röstade issues

--- src/components/FetchIssue.js
+++ src/components/FetchIssue.js
@@ -2,18 +2,21 @@ function FetchIssue(number){
 
     /*Send in the number of the issue you want to get, maybe*/
     
-    let contents={
-        "issueId": 1,
-        "header": "Rubrik",
-        "description": "Förklarande text angående issuet",
-        "done": false,
-        "votes": [2, 4, 3, 7, 9, 5,10,15,3]
-    }
+    // let contents={
+    //     "issueId": 1,
+    //     "header": "Rubrik",
+    //     "description": "Förklarande text angående issuet",
+    //     "done": false,
+    //     "votes": [2, 4, 3, 7, 9, 5,10,15,3]
+    // }
+
+    // Hämtar issues som är färdigissade från TimeEstimated  
+    let contents = number.issue;
     
-    const sum = contents.votes.reduce((a, b) => a + b, 0);
-    const avg = (sum / contents.votes.length) || 0;
+    const sum = contents.hours.reduce((a, b) => a + b, 0);
+    const avg = (sum / contents.hours.length) || 0;
     const avgRounded = Math.round(avg * 100) / 100;
-    const sorted = contents.votes.slice().sort((a, b) => a - b);
+    const sorted = contents.hours.slice().sort((a, b) => a - b);
     const middle = Math.floor(sorted.length / 2);
     
     return(
