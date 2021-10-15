acf89335e71f613fdd955bc989222a7d74baece4
Judith Gammelli <60819967+JGammelli@users.noreply.github.com>
2021-10-15 10:51:55 +0200

    printguessed added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import FirstPage from './components/FirstPage';
 import AddedIssue from './components/AddedIssue';
 import NewIssue from './components/NewIssue';
 import React, { useState } from 'react';
+import PrintGuessedIssues from './components/PrintGuessedIssues';
 
 function App() {
     const [user, setUser] = useState(null);
@@ -15,6 +16,7 @@ function App() {
                 <div>
                     <AddedIssue />
                     <NewIssue />
+                    <PrintGuessedIssues />
                 </div>
             )}
         </div>
